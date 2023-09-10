1a4d47cdba1bbe3e137629afcc62455805b665d7
Kimmi17 <dangthikimthuybl@gmail.com>
2023-09-10 16:25:31 +0300

    Adding interface Customer to types

--- types.ts
+++ types.ts
@@ -41,6 +41,12 @@ export interface Price {
   metadata?: Stripe.Metadata;
   products?: Product;
 }
+
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
+
 export interface ProductWithPrice extends Product {
   prices?: Price[];
 }
