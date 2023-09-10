6848201113eff0233882fcae3e341f3dbb5926c1
Kimmi17 <dangthikimthuybl@gmail.com>
2023-09-10 16:09:39 +0300

    Correct file name

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,8 +1,11 @@
 "use client";
+
 import Image from "next/image";
+
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
-import PlayButton from "./PalyButton";
+
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   data: Song;
@@ -11,6 +14,7 @@ interface SongItemProps {
 
 const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
   const imagePath = useLoadImage(data);
+
   return (
     <div
       onClick={() => onClick(data.id)}
